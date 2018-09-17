13ff8e7b2bf2d99f094ad7fba3006ba293c7562a
bkonyi <bkonyi@google.com>
2018-09-16 18:00:22 -0700

    Added icons and cleanup

--- lib/src/geofence_trigger.dart
+++ lib/src/geofence_trigger.dart
@@ -31,40 +31,60 @@ abstract class GeofenceTrigger {
   static bool _isInitialized = false;
   static StreamSubscription _locationUpdates;
 
-  static Future<void> _handleLocationUpdate(Position p) async {
-    final home = homeRegion.location;
-    final distance = await Geolocator().distanceBetween(
-        p.latitude, p.longitude, home.latitude, home.longitude);
-    print('$p : $home');
-    print('Distance to home: $distance');
-    if (distance < 100.0) {
-      await postNotification(0, 'Opening Garage Door',
-          'A geofence event has triggered the garage door!');
-      await GarageDoorRemote.openDoor();
-      await _locationUpdates.cancel();
-      _locationUpdates = null;
-    }
-  }
-
-  static final homeRegion = GeofenceRegion('home', 0.0, 0.0, 10000.0,
-      <GeofenceEvent>[GeofenceEvent.enter, GeofenceEvent.exit],
-      androidSettings: _androidSettings);
-
-  static Future<void> homeGeofenceCallback(
-      List<String> id, Location location, GeofenceEvent event) async {
+  static Future<void> _initialize() async {
     if (!_isInitialized) {
       await initialize();
       _notificationPlugin.initialize(_initializationSettings);
       _isInitialized = true;
     }
+  }
+
+  static Future<void> _startUpdates() async {
+    print('Starting location updates');
+    await GeofencingManager.promoteToForeground();
+    _locationUpdates =
+        (await Geolocator().getPositionStream()).listen(_handleLocationUpdate);
+  }
+
+  static Future<void> _stopUpdates() async {
+    await _locationUpdates?.cancel();
+    _locationUpdates = null;
+    await GeofencingManager.demoteToBackground();
+  }
+
+  static Future<void> _handleLocationUpdate(Position p) async {
+    final home = homeRegion.location;
+    final distance = await Geolocator().distanceBetween(
+        p.latitude, p.longitude, home.latitude, home.longitude);
+    print('Distance to home: $distance');
+    if (distance < 100.0) {
+      if (!(await GarageDoorRemote.isOpen)) {
+        await postNotification(0, 'Opening Garage Door',
+            'A geofence event has triggered the garage door!');
+        await GarageDoorRemote.openDoor();
+      }
+      await _stopUpdates();
+    }
+  }
+
+  static final homeRegion = GeofenceRegion(
+      'home',
+      0.0,
+      0.0,
+      1000.0,
+      <GeofenceEvent>[
+        GeofenceEvent.enter,
+        GeofenceEvent.exit,
+      ],
+      androidSettings: _androidSettings);
+
+  static Future<void> homeGeofenceCallback(
+      List<String> id, Location location, GeofenceEvent event) async {
+    await _initialize();
     if (event == GeofenceEvent.enter) {
-      print('Starting location updates');
-      _locationUpdates = (await Geolocator().getPositionStream())
-          .listen(_handleLocationUpdate);
+      await _startUpdates();
     } else if ((event == GeofenceEvent.exit) && (_locationUpdates != null)) {
-      print('Cancelling location updates');
-      await _locationUpdates.cancel();
-      _locationUpdates = null;
+      await _stopUpdates();
     }
   }
 }
